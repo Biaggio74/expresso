f2dc6eb7d8b7c888005d3f6c25e74c478f9d0afc
BKI <b.kisfali@gmail.com>
2018-01-22 18:45:13 +0100

    update up to employee PUR request

--- api/employees.js
+++ api/employees.js
@@ -56,7 +56,47 @@ employeesRouter.post('/', (req, res, next) => {
         }
       })
     }
-  })
+  });
+});
+
+employeesRouter.put('/:id', (req, res, next) => {
+  if (!req.body.employee.name || !req.body.employee.position || !req.body.employee.wage){
+    res.status(400).send('Invalid employee');
+  }
+  const sql = `UPDATE Employee
+  SET
+    name = $name,
+    position = $position,
+    wage = $wage
+  WHERE Employee.id = ${req.params.id}`;
+  const data = {
+    $name: req.body.employee.name,
+    $position: req.body.employee.position,
+    $wage: req.body.employee.wage
+  };
+  db.run(sql, data, (error) => {
+    if (error){
+      res.status(500).send();
+    } else {
+      db.get(`SELECT * FROM Employee WHERE Employee.id = ${req.params.id}`,(err,row) => {
+        if (err){
+          res.status(500).send();
+        } else if (row){
+          res.status(200).send({employee: row});
+        } else {
+          res.status(404).send();
+        }
+      });
+    }
+  }); //END of db.run
 })
+
+
+
+
+
+
+
+
 
 module.exports = employeesRouter;
